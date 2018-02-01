d87e84b1395e223326a5b283371f178fe7e8da02
Alexander Grushevoy <alexander.grushevoy@gmail.com>
2018-02-01 08:33:40 +0200

    Create project

---
+++ INFView/INFView.h
@@ -0,0 +1,19 @@
+//
+//  INFView.h
+//  INFView
+//
+//  Created by Alexander on 2/1/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for INFView.
+FOUNDATION_EXPORT double INFViewVersionNumber;
+
+//! Project version string for INFView.
+FOUNDATION_EXPORT const unsigned char INFViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <INFView/PublicHeader.h>
+
+
---
+++ INFViewTests/INFViewTests.m
@@ -0,0 +1,39 @@
+//
+//  INFViewTests.m
+//  INFViewTests
+//
+//  Created by Alexander on 2/1/18.
+//  Copyright © 2018 Alexander. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface INFViewTests : XCTestCase
+
+@end
+
+@implementation INFViewTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
